7a03bcd00f597c0bc46b7422297a2fa240b9fd16
arihant agarwal <arihant2002agarwal@gmail.com>
2024-06-19 01:25:05 +0530

    final push

--- app/(dashboard)/[storeId]/layout.tsx
+++ app/(dashboard)/[storeId]/layout.tsx
@@ -13,14 +13,14 @@ export default async function DashboardLayout({
 }){
     const {userId } = auth();
 
-    if(!useId){
+    if(!userId){
         redirect('/sign-in');
     }
 
     const store = await prismadb.store.findFirst({
         where:{
             id:params.storeId,
-            userId
+            userId:userId
         }
     });
     
